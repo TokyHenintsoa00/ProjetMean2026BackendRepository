2e779825f9fd0d8b0eea197433e3aee8c1d7244e
TokyHenintsoa00 <tokyramanalinarivo@gmail.com>
2026-02-19 01:22:45 +0300

    copie midlaew depuis main

--- Middleware/verifyToken.js
+++ Middleware/verifyToken.js
@@ -9,15 +9,18 @@ const {verifyToken} = require('../utils/TokenConfig');
 
 
 const authMiddleware = (req, res, next) => {
-    const token = req.cookies.token; // le token directement, pas de "Bearer"
+    const token = req.cookies.token_user;
+
     if (!token) {
         return res.status(401).json({ message: "Token manquant" });
     }
+
     const decoded = verifyToken(token);
     if (!decoded) {
         return res.status(401).json({ message: "Token invalide" });
     }
-    req.user = decoded;
+
+    req.user = decoded; // id, email, role, id_boutique_user...
     next();
 };
 
